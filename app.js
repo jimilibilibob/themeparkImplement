3163887757fc07e0b78323b34ff21bcb4953af26
Thomas <thomas.fontaine@lateos.com>
2019-03-25 15:04:51 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,88 @@
+
+const Themeparks = require("themeparks");
+
+const disneyMagicKingdom = new Themeparks.Parks.DisneylandParisMagicKingdom();
+
+const elasticsearch = require('elasticsearch');
+
+const dateFormat = require('dateformat');
+
+const client = new elasticsearch.Client({
+    host: 'https://search-data-bison-mn7mmzhqmda53plwq7jolphcqu.eu-west-3.es.amazonaws.com'
+});
+
+const uuidv4 = require('uuid/v4');
+
+
+client.indices.delete({
+    index: 'disney',
+    ignore: [404,400]
+}).then(function () {
+    console.log('index deleted or never existed');
+    create()
+}, function (error) {
+    console.log(error)
+});
+
+
+function create(){
+    client.indices.create({
+        index: 'disney',
+        ignore: [404,400]
+    }).then(function () {
+        console.log('index created or already existed');
+        createMapping()
+    }, function (error) {
+        console.log(error)
+    });
+}
+
+var tid = null ;
+
+function createMapping(){
+    let mapping = {"waitTime":{"properties":{"attraction":{"type":"keyword"},"dateTime":{"type":"date","format":"YYYY-MM-dd HH:mm:ss"},"attente":{"type":"integer"},"position":{"type":"geo_point"}}}}
+
+    client.indices.putMapping({index:"disney", type:"waitTime", body:mapping, ignore: [404,400]}).then(function () {
+        console.log('mapping created ');
+        tid = setTimeout(getTime, 2000);
+    }, function (error) {
+        console.log(error)
+    });
+}
+
+
+
+
+function getTime(){
+    let dateTime = new Date();
+    let changeDate = Math.floor(dateTime.getTime()/(1000*60*10))*(1000*60*10);
+    let formate = dateFormat(new Date(changeDate),"yyyy-mm-dd HH:MM:ss");
+    disneyMagicKingdom.GetWaitTimes().then(function(rides) {
+        for(var i=0, ride; ride=rides[i++];) {
+            let result = {
+                "attraction":ride.name,
+                "attente":ride.waitTime,
+                "dateTime":formate
+            };
+            console.log(result);
+            addToES(result);
+        }
+
+    }, console.error);
+    tid = setTimeout(getTime, 1000 * 60 * 10 );
+}
+
+
+
+function addToES(results){
+     client.create({
+         index: 'disney',
+         type: 'waitTime',
+         id : uuidv4(),
+         body: JSON.stringify(results)
+    }, function (err) {
+         if(err) {
+             addToES(results)
+         }
+     });
+}
